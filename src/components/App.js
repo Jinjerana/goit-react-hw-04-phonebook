b7d529dd61692dca43cd86dc428c3736c0903ce5
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-09-20 22:27:57 +0200

    4

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,18 @@ import Forma from './Form';
 import ContactList from './Contacts/Contacts';
 import Filter from './Filter';
 
-export default function App() {
+export function App() {
   // props for contacts
-const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ?? '')
+  // const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
+  const [contacts, setContacts] = useState(
+    JSON.stringify(window.localStorage.getItem('contacts')) ?? ''
+  );
   // props for filter
-  const [filter, setFilter] = useState(true);
+  const [filter, setFilter] = useState('');
 
-
-// export class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-
-useEffect(() => {
-  window.localStorage.setItem('contacts', JSON.parse(contacts))
-}, [contacts]);
-
-
-
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.parse(contacts));
+  }, [contacts]);
 
   // componentDidMount = () => {
   //   const savedContacts = localStorage.getItem('contacts');
@@ -30,25 +23,19 @@ useEffect(() => {
   //   this.setState({ contacts: JSON.parse(savedContacts) });
   // };
 
-  componentDidUpdate = (prevProps, prevState) => {
-    console.log('update');
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
+  // componentDidUpdate = (prevProps, prevState) => {
+  //   console.log('update');
+  //   if (prevState.contacts !== this.state.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== contactId
-        ),
-      };
-    });
-  };
-
-  addContact = newContact => {
-    const isSameContact = this.state.contacts.some(
+  const addContact = newContact => {
+    const isSameContact = contacts.some(
       ({ name, number }) =>
         name.toLowerCase() === newContact.name.toLowerCase() ||
         number === newContact.number
@@ -59,36 +46,26 @@ useEffect(() => {
       return;
     }
 
-    this.setState(prevState => {
-      return {
-        contacts: [newContact, ...prevState.contacts],
-      };
-    });
+    setContacts(contacts => [...newContact, ...contacts]);
   };
 
-  changeFilter = e => {
-    setFilter(e.currentTarget.value.toLowerCase());
-    // this.setState({ filter: e.currentTarget.value.toLowerCase() });
+  const changeFilter = e => {
+    setFilter(e.target.value.toLowerCase());
   };
 
-  getVisibleContacts = () => {
-    // const { filter, contacts } = this.state;
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter)
-    );
+  const getVisibleContacts = () => {
+    contacts.filter(contact => contact.name.toLowerCase().includes(filter));
   };
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <Forma onAddContact={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={changeFilter} />
-        <ContactList onFilter={visibleContacts} onDelete={this.deleteContact} />
-      </div>
-    );
-  }
+  // render() {
+  //   const filter = this.state;
+  //   const visibleContacts = this.getVisibleContacts();
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Forma onAddContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList onFilter={getVisibleContacts()} onDelete={deleteContact} />
+    </div>
+  );
 }
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { List, Item, Button } from './ContactsStyled';
 
-const ContactList = ({ onFilter, onDelete }) => {
-  return (
-    <List>
-      {onFilter.map(({ name, number, id }) => {
-        return (
-          <Item key={id}>
-            <span>{name}:</span>
-            <span>{number}:</span>
+const ContactList = ({ onFilter, onDelete }) => (
+  <List>
+    {onFilter.map(({ name, number, id }) => {
+      return (
+        <Item key={id}>
+          <span>{name}:</span>
+          <span>{number}:</span>
 
-            <Button type="button" onClick={() => onDelete(id)}>
-              Delete
-            </Button>
-          </Item>
-        );
-      })}
-    </List>
-  );
-};
+          <Button type="button" onClick={() => onDelete(id)}>
+            Delete
+          </Button>
+        </Item>
+      );
+    })}
+  </List>
+);
 
 export default ContactList;
