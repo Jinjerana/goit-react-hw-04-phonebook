c8580a022cd6549f16cf15c99bf63b78e9d82b7e
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-09-23 00:52:26 +0200

    5

--- src/components/App.js
+++ src/components/App.js
@@ -5,30 +5,15 @@ import ContactList from './Contacts/Contacts';
 import Filter from './Filter';
 
 export function App() {
-  // props for contacts
-  // const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
   const [contacts, setContacts] = useState(
-    JSON.stringify(window.localStorage.getItem('contacts')) ?? ''
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
   );
-  // props for filter
-  const [filter, setFilter] = useState('');
+
+  const [filter, setFilter] = useState(' ');
 
   useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.parse(contacts));
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
-  // componentDidMount = () => {
-  //   const savedContacts = localStorage.getItem('contacts');
-  //   if (!savedContacts) return;
-  //   this.setState({ contacts: JSON.parse(savedContacts) });
-  // };
-
-  // componentDidUpdate = (prevProps, prevState) => {
-  //   console.log('update');
-  //   if (prevState.contacts !== this.state.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // };
 
   const deleteContact = contactId => {
     setContacts(contacts.filter(contact => contact.id !== contactId));
@@ -46,19 +31,15 @@ export function App() {
       return;
     }
 
-    setContacts(contacts => [...newContact, ...contacts]);
+    setContacts(contacts => [newContact, ...contacts]);
   };
 
   const changeFilter = e => {
     setFilter(e.target.value.toLowerCase());
   };
 
-  const getVisibleContacts = () => {
+  const getVisibleContacts = () =>
     contacts.filter(contact => contact.name.toLowerCase().includes(filter));
-  };
-  // render() {
-  //   const filter = this.state;
-  //   const visibleContacts = this.getVisibleContacts();
   return (
     <div>
       <h1>Phonebook</h1>
